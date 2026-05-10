d229bbbe3808b6bdc5baa28c01767e3459c1dfcd
Elena Atero <eatero@outlook.es>
2026-05-10 18:28:27 +0000

    errores

--- models/marts/dim_segmento_cliente.sql
+++ models/marts/dim_segmento_cliente.sql
@@ -4,29 +4,33 @@ with clientes as (
 
 ),
 
+canales as (
+
+    select cod_canal, canal
+    from {{ ref('stg_bronze__canales_raw') }}
+
+),
+
 segmentos as (
 
     select distinct
-        segmento_edad,
-        canal_preferido
-    from clientes
-    where es_anonimo = false
-      and segmento_edad    is not null
-      and canal_preferido  is not null
+        cl.segmento_edad,
+        ca.canal                                                 as canal_preferido
+    from clientes cl
+    left join canales ca
+        on cl.cod_canal_preferido = ca.cod_canal
+    where cl.es_anonimo = false
+      and cl.segmento_edad        is not null
+      and cl.cod_canal_preferido  is not null
 
 ),
 
 dim as (
 
     select
-        -- clave surrogada — necesaria porque la clave es compuesta
-        {{ dbt_utils.generate_surrogate_key(['segmento_edad', 'canal_preferido']) }}  as segmento_sk,
-
-        -- atributos
+        {{ dbt_utils.generate_surrogate_key(['segmento_edad', 'canal_preferido']) }} as segmento_sk,
         segmento_edad,
         canal_preferido,
-
-        -- metadatos
         current_timestamp()                                      as _loaded_at
 
     from segmentos
