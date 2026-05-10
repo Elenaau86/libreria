db07e32998e851d26f4b915338a77bc8ede4d5da
Elena Atero <eatero@outlook.es>
2026-05-10 21:33:34 +0000

    corrigiendo municipios

--- models/staging/stg_bronze__provincias_raw.sql
+++ models/staging/stg_bronze__provincias_raw.sql
@@ -7,10 +7,10 @@ with source as (
 dim as (
 
     select distinct
-        cod_provincia_ine,
-        provincia,
-        ccaa,
-        count(*) over (partition by cod_provincia_ine)           as num_municipios
+        try_cast(cod_provincia_ine as integer)                    as cod_provincia_ine,
+        trim(provincia)                                           as provincia,
+        try_cast(cod_ccaa_ine as integer)                         as cod_ccaa_ine,
+        count(*) over (partition by trim(provincia))              as num_municipios
 
     from source
     where provincia is not null
